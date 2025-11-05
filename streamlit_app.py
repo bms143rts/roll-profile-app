59d0692db44a686b8b972434ac4b409a36b7de9e
bms143rts <bm.sarkar.22@gmail.com>
2025-11-05 15:01:15 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -593,7 +593,8 @@ try:
     html_table = render_merged_roll_table(plot_df, selected_roll)
     st.markdown(html_table, unsafe_allow_html=True)
 
-    
+    except Exception as e:
+    st.error(f"Could not render formatted table: {e}")
 
 
                             # pivot table
@@ -602,6 +603,7 @@ try:
 
 
                           
+
 
 
 
